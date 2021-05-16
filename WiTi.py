d2948b756474364b2d1f998dc28030892e0e82d0
pawel <jpawlacz@tlen.pl>
2021-05-16 13:06:52 +0200

    ordered crossover function added

--- WiTi.py
+++ WiTi.py
@@ -124,6 +124,21 @@ class Task:
         self.w = w
 
 
+# def Ce(j, data, n):
+#     # return sum([data[i].p for i in range(n) if j & (1 << i)])
+#     C = [data[i].p for i in range(n)]
+#     C.insert(0, 0)
+#     B = [data[i].p + C[i - 1] for i in range(1, n) if j & (1 << i)]
+#     print(C)
+#     print(B)
+#     return sum(B)
+
+
+# C += [data[i].p + C[i - 1] for i in range(1, n) if j & (1 << i)]
+# return sum(C)
+# return sum([data[i].p for i in range(n) if j & (1 << i)])
+
+
 def SMTWT(n, data):
     OPT_tab = [0]
     perm = 2 ** n
@@ -155,6 +170,10 @@ def old(n, data):
     return OPT_tab[perm - 1]
 
 
+# for i in range(10):
+#     print(5 & (1 << i))
+
+
 def takeSecond(elem):
     return elem[1]
 
@@ -168,9 +187,10 @@ class Genetic():
         self.best_sequence = []
         self.data = data
         self.n = len(data)
-        self.best = []
-        self.medium = []
-        self.weak = []
+        self.best=[]
+        self.medium=[]
+        self.weak=[]
+
 
     def WiTi(self, C, w, d):
         return max(0, C - d) * w
@@ -180,32 +200,81 @@ class Genetic():
         return [sum(pe[:i]) for i in range(1, self.n + 1)]
 
     def crossingOperator(self, r1, r2):
-        tmp1, tmp2 = randint(0, self.n), randint(0, self.n)
-        idx1 = min(tmp1, tmp2)
-        idx2 = max(tmp1, tmp2)
+        idx1, idx2 = 2, 6
         return r1[:idx1] + r2[idx1:idx2] + r1[idx2:], r2[:idx1] + r1[idx1:idx2] + r2[idx2:]
+
+    def orderedCrossover(self, mum, dad):
+
+        size = len(mum)
+
+        # Choose random start/end position for crossover
+        alice, bob = [-1] * size, [-1] * size
+        start, end = sorted([random.randrange(size) for _ in range(2)])
+
+        # print(start, end)
+        # Replicate mum's sequence for alice, dad's sequence for bob
+        alice_inherited = []
+        bob_inherited = []
+        for i in range(start, end + 1):
+            alice[i] = mum[i]
+            bob[i] = dad[i]
+            alice_inherited.append(mum[i])
+            bob_inherited.append(dad[i])
+
+        # Fill the remaining position with the other parents' entries
+        current_dad_position, current_mum_position = 0, 0
+
+        fixed_pos = list(range(start, end + 1))
+        i = 0
+        while i < size:
+            if i in fixed_pos:
+                i += 1
+                continue
+
+            test_alice = alice[i]
+            if test_alice == -1:  # to be filled
+                dad_trait = dad[current_dad_position]
+                while dad_trait in alice_inherited:
+                    current_dad_position += 1
+                    dad_trait = dad[current_dad_position]
+                alice[i] = dad_trait
+                alice_inherited.append(dad_trait)
+
+            test_bob = bob[i]
+            if test_bob == -1:  # to be filled
+                mum_trait = mum[current_mum_position]
+                while mum_trait in bob_inherited:
+                    current_mum_position += 1
+                    mum_trait = mum[current_mum_position]
+                bob[i] = mum_trait
+                bob_inherited.append(mum_trait)
+
+            # repeat block for bob and mom
+            i += 1
+
+        return alice, bob
+
 
     def purposeFunc(self, Pi):
         c = self.cFunc(Pi)
-        return sum([self.WiTi(c[i], data[Pi[i]].w, data[Pi[i]].d) for i in range(len(Pi))])
+        return sum([self.WiTi(c[i], data[Pi[i].w], data[Pi[i]].d)])
 
     def rmvItem(self, list1, list2):
-        rm1 = random.choice(list1)
+        rm1, rm2 = random.choice(list1), random.choice(list2)
         list1.remove(rm1)
-        rm2 = random.choice(list2)
         list2.remove(rm2)
         return (rm1, rm2), list1, list2
 
-    def makeParents(self):
+    def tmp(self):
         dict = {1: self.best, 2: self.medium, 3: self.weak}
         if len(self.best) and len(self.medium):
             a = dict[randint(1, 2)]
         elif len(self.best) and not len(self.medium):
             a = dict[1]
-        elif not len(self.best) and len(self.medium):
+        elif not len(self.best) and  len(self.medium):
             a = dict[2]
-        elif len(self.weak) > 1:
-            a = dict[3]
+        elif len(self.weak)>1:
+            a=dict[3]
         else:
             return "xd"
         if len(self.best) and len(self.medium) and len(self.weak):
@@ -220,95 +289,88 @@ class Genetic():
             b = dict[3]
         else:
             return "xd"
-        if a == b:
-            if len(a) > 1:
-                t, a, b = self.rmvItem(a, b)
-                self.R.append(t)
-            else:
-                pass
-        else:
-            t, a, b = self.rmvItem(a, b)
-            self.R.append(t)
+
+        self.R, a, b = self.rmvItem(a, b)
 
     def pickParents(self, perms):
         idx1, idx2 = int(len(perms) / 3), int(len(perms) / 3 * 2)
         perms.sort(key=takeSecond)
         self.best, self.medium, self.weak = perms[:idx1], perms[idx1:idx2], perms[idx2:]
-        while (len(self.best) + len(self.medium) + len(self.weak)) > 1:
-            self.makeParents()
+        print("best",self.best)
+        print("med", self.medium)
+        print("weak",self.weak)
+        print(idx1, idx2, len(perms))
+        # a, best, weak = self.rmvItem(best, weak)
+        print("------------------------")
 
-    def updateBestForChild(self):
-        for c in self.C:
-            if c[1] < self.best_value:
-                self.best_value = c[1]
-                self.best_sequence = list.copy(c[0])
-            c_perm, c_val, boo = self.mutating(c)
-            if boo:
-                self.C.append((c_perm, c_val))
-                if c_val < self.best_value:
-                    self.best_value = c_val
-                    self.best_sequence = list.copy(c_perm)
-
-    def swap(self, data):
-        x1, x2 = randint(0, len(data) - 1), randint(0, len(data) - 1)
-        data[x1], data[x2] = data[x2], data[x1]
-        return data
-
-    def mutating(self, child):
-        if randint(1, 100) < 5:
-            c = self.swap(child[0])
-            c_val = self.purposeFunc(c)
-            return c, c_val, True
-        else:
-            return child[0], child[1], False
-
-    def selection(self):
-        Tmp = self.X + self.C
-        Tmp.sort(key=takeSecond)
-        self.X.clear()
-        self.C.clear()
-        self.R.clear()
-        self.X = Tmp[:int(self.n * 0.2)] + random.sample(Tmp[int(self.n * 0.2):], int(self.n * 0.81))
+        # a=(random.choice(best),random.choice(weak))
+        while (len(self.best) +len(self.medium) + len(self.weak))>1:
+            self.tmp()
+            # self.R, a, b = self.rmvItem(a, b)
+        print("best", self.best)
+        print("med",  self.medium)
+        print("weak", self.weak)
+        print(idx1, idx2, len(perms))
+        print('a', a)
+        print(self.R)
 
 
-    def initialPerm(self, Pi, p):
-        old_sequence = list.copy(Pi)
-        self.best_sequence = list.copy(Pi)
-        old_value = self.purposeFunc(old_sequence)
-        self.best_value = old_value
-        self.X.append((old_sequence, old_value))
-        for i in range(1, p):
-            new_sequence = list.copy(old_sequence)
-            shuffle(new_sequence)
-            new_value = self.purposeFunc(new_sequence)
-            self.X.append((new_sequence, new_value))
-            if new_value < old_value:
-                self.best_value = new_value
-                self.best_sequence = list.copy(new_sequence)
-            old_sequence, old_value = list.copy(new_sequence), new_value
-
-    def makeChildren(self):
-        for i in range(len(self.R)):
-            c1, c2 = self.crossingOperator(self.R[i][0][0], self.R[i][1][0])
-            c1_val, c2_val = self.purposeFunc(c1), self.purposeFunc(c2)
-            self.C.append((c1, c1_val))
-            self.C.append((c2, c2_val))
+print(random.randrange(1, 3, 2))
 
 
-
-def makeChildren(p, Pi, data):
+def initialPerm(Pi, p, data):
     sol = Genetic(data)
-    sol.initialPerm(Pi, p)
-    for i in range(5):
-        sol.pickParents(sol.X)
-        sol.makeChildren()
-        sol.updateBestForChild()
-        sol.selection()
-    print("best is ",sol.best_value,sol.best_sequence)
+    sol.best_sequence = old_sequence = Pi
+    sol.best_value = old_value = sol.purposeFunc(old_sequence)
+    sol.X.append([old_sequence, old_value])
+    for i in range(1, p):
+        new_sequence = old_sequence
+        shuffle(new_sequence)
+        new_value = sol.purposeFunc(new_sequence)
+        if new_value < old_value:
+            sol.best_value = new_value
+        old_sequence, old_value = new_sequence, new_value
+        sol.X.append([new_sequence, new_value])
+
+
+def alg(p, Pi):
+    x = 1
 
 
 if __name__ == '__main__':
     result_tab = []
+
     data = ReadFile()
-    n = len(data)
-    makeChildren(n, [*range(n)], data)
+    print(len(data))
+    a = [1, 2, 3, 4, 5, 6, 7, 8]
+    shuffle(a)
+    print(a)
+    # n = len(data)
+    # print(data)
+    # result = WiTi(n, data)
+    # result_tab.append(result)
+    # print(toCe(data, n))
+    # print('wynikiWITI: ')
+    # print(result_tab)
+    # number_list = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70]
+    # print("Original list : ", number_list)
+    #
+    # random.shuffle(number_list)  # shuffle method
+    # print("List after shuffle  : ", number_list)
+    # print(crossingOperator([10, 11, 12, 13, 14, 15, 16, 17, 18, 19], [*range(20, 30)]))
+    A = [[[1, 2, 13, 41, 5, 61, 7], 7], [[1, 2, 13, 41, 51, 6, 71], 6], [[11, 2, 13, 4, 51, 6, 7], 3],
+         [[1, 21, 3, 41, 5, 61, 7], 4], [[1, 2, 3, 4, 15, 6, 17], 1], [[1, 12, 3, 41, 5, 6, 7], 2],
+         [[1, 12, 3, 41, 5, 6, 7], 9], ]
+
+    g = Genetic(data)
+    g.pickParents(A)
+# a = ['asd', 'asdf', 'asdft4', 'urihhs']
+# b = ['11', '22', '3', '44']
+# v = set(a)
+# print(v)
+#
+# print([[x, c] for x, c in zip(a, b)])
+
+# a = [1]
+# a += [a[i]+1 for i in range(10)]
+# print(a)
